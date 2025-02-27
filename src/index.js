88aa42a09719907d64af1c0673a1c34d4ff744e7
Abhiram <abhiramnaidu2003@gmail.com>
2025-02-27 19:34:46 +0530

    routers added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const bodyParser=require('body-parser');
 const {PORT}=require('./config/serverconfig');
 
+const APIRoutes=require('./routes/index');
 
 const setupAndStartServer=async()=>{
 
@@ -9,6 +10,7 @@ const setupAndStartServer=async()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',APIRoutes);
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
        
---
+++ src/repository/index.js
@@ -0,0 +1,5 @@
+const CityRepository = require("./city_repository");
+
+module.exports={
+    CityRepository:require('./city_repository')
+};
