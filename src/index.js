9952f3e1d1a7e2f47c94f8ee6c2cedc4e9c5acf6
Abhiram <abhiramnaidu2003@gmail.com>
2025-03-01 11:28:14 +0530

    added flights model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const bodyParser=require('body-parser');
 const {PORT}=require('./config/serverconfig');
 const APIRoutes=require('./routes/index');
-const {Airport,City}=require('./models/index');
+const {Airport,City,Airplane}=require('./models/index');
 const db=require('./models/index');
 const setupAndStartServer=async()=>{
     // create the express object
@@ -11,14 +11,9 @@ const setupAndStartServer=async()=>{
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',APIRoutes);
     app.listen(PORT,async ()=>{
-        console.log(`Server started at ${PORT}`); 
-        const city=await City.findOne({
-            where:{
-                id:4
-            }
-        });
-        const airports=await city.getAirports();
-        console.log(city,airports);
+        await Airplane.create({
+            modelNumber:'Bombardier CRJ'
+        })
     });
 }
 setupAndStartServer();
